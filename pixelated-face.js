fe431af068d22cc89fa82cbbe52b54f9ce419ab1
CheyenneClarkZone <57722115+CheyenneClarkZone@users.noreply.github.com>
2020-01-15 13:50:02 +0000

    Added .length so console tells you how many faces are detected

--- pixelated-face.js
+++ pixelated-face.js
@@ -16,7 +16,7 @@ async function populateVideo() {
   video.srcObject = stream;
   await video.play();
   //size the canvases to be the same size as video
-  console.log(video.videoWidth, video,videoHeight);
+  console.log(video.videoWidth, video.videoHeight);
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   faceCanvas.width = video.videoWidth;
@@ -25,9 +25,9 @@ async function populateVideo() {
 
 async function detect() {
   const faces = await faceDetector.detect(video); //new function to detect faces in the shot. New variable called faces, call await on it so it doesn't run until a promise is settled, then call to detect the face on faceDetector. Pass to that a video.
-  console.log(faces);
+  console.log(faces.length);
   //ask the browser when the next animation frame is, and tell it to run detect for us.
-  requestAnimationFrame(detect); //pass requestAnimationFrame a detect. Recursion - detect is being called from within detect, and that allows us to just be running it infinitely.
+  requestAnimationFrame(detect); //pass requestAnimationFrame a detect. Recursion - detect is being called from within detect, and that allows us to just be running it infinitely. 
 }
 
 populateVideo().then(detect);  //need to run .then after the video has been populated, because if you run detect when there's no video it won't find faces. It is a promise base.
