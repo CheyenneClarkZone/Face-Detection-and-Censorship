cde1c57a40d8f04412f2c07e4867118b131db3c9
CheyenneClarkZone <57722115+CheyenneClarkZone@users.noreply.github.com>
2020-01-15 12:21:51 +0000

    Ask the browser when the next animation frame is, and tell it to run detect for us.

--- pixelated-face.js
+++ pixelated-face.js
@@ -26,6 +26,8 @@ async function populateVideo() {
 async function detect() {
   const faces = await faceDetector.detect(video); //new function to detect faces in the shot. New variable called faces, call await on it so it doesn't run until a promise is settled, then call to detect the face on faceDetector. Pass to that a video.
   console.log(faces);
+  //ask the browser when the next animation frame is, and tell it to run detect for us.
+  requestAnimationFrame(detect); //pass requestAnimationFrame a detect. Recursion - detect is being called from within detect, and that allows us to just be running it infinitely.
 }
 
-populateVideo().then(detect); //need to run .then after the video has been populated, because if you run detect when there's no video it won't find faces. It is a promise base.
+populateVideo().then(detect);  //need to run .then after the video has been populated, because if you run detect when there's no video it won't find faces. It is a promise base.
